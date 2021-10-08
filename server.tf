c387082e042b3151d4bbb447b0ceb7057cc52197
GRanjeev <88188003+GRanjeev@users.noreply.github.com>
2021-10-08 18:39:10 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -4,7 +4,7 @@ region = "ap-south-1"
 resource "aws_instance" "myawsserver" {
   ami = "ami-026f33d38b6410e30"
   instance_type = "t2.micro"
-  key_name = "terra1"
+  key_name = "test"
 
   tags = {
     Name = "Greeshma-devops-ec2-instance"
